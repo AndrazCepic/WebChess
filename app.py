d9a3340daff9c87efc4699358987ae4482a7a210
Andr4zC3p1C <andraz.cepic@gmail.com>
2019-09-10 03:58:44 +0200

    Struktura modulov

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from bottle import route, run
+from bottle import route, run, template
 from engine.igra import Igra
 
 # TEST
 stran = '<!DOCTYPE html><h1 style="">HELLO WORLD</h1>'
-
 igra = Igra()
 
-@route('/hello')
-def hello():
-    return stran
+@route('/')
+def index():
+    return template("index")
 
-run(host='localhost', port=8080, debug=True)
+run(host='localhost', port=8080, debug=True, reloader=True)
